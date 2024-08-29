d48ac0ab49c9d22a01ccfaaba72c00209b295368
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-08-29 21:35:34 +0200

    persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,34 @@ import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const contactsConfig = {
+  key: "contactsKey",
+  storage,
+  whitelist: ["items"], // blacklist: ["showProfilesList"]
+};
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(contactsReducer, contactsConfig),
     filters: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
